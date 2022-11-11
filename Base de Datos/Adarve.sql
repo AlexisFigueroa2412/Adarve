0e870b21949b32a24513a110dc1b1d76da52cf49
Alepo2412 <onedreamermore24.12@gmail.com>
2022-11-11 12:36:52 -0600

    Create adarve.sql

---
+++ Base de Datos/Adarve.sql
@@ -0,0 +1,12 @@
+-- Database: adarve
+
+-- DROP DATABASE adarve;
+
+CREATE DATABASE adarve
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.utf8'
+    LC_CTYPE = 'English_United States.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
